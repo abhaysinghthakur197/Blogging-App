20360938a219e776a18bdd3ff1645dbbb1fcc078
Abhay Singh <59767465+abhaysinghthakur197@users.noreply.github.com>
2023-12-19 00:08:00 +0530

    added signup page, connected mongodb, done hashing of password

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express')
 const path = require('path')
 
+// User Route
+const userRoute = require('./routes/user')
+//  *** //
+
+// Connect MongoDB
+const { connectToMongoDB } = require('./connect')
+// *** //
 const app = express();
 const PORT = 8000;
 
+// MongoDB connect
+connectToMongoDB("mongodb://127.0.0.1:27017/blogging-app").then(() => console.log("Db connected")).catch((e) => console.log(e));
+//  *** //
+
+// view engine - EJS
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
+// *** //
 
 
-app.use('/',(req,res) =>{
+// middleware -> 
+// To work with form data 
+app.use(express.urlencoded({extended: false}))
+// *** //
+
+
+app.get('/',(req,res) =>{
     res.render('Home')
 })
+
+app.use("/user",userRoute);
+
 app.listen(PORT, () =>
     console.log(`Server started at PORT:${PORT}`)
 );
--- models/user.js
+++ models/user.js
@@ -1,41 +1,64 @@
 const { Schema, model } = require('mongoose')
 
+// crypto node js package for password hashing
+const {createHmac,randomBytes} = require('crypto')
+//  *** //
+
 const userSchema = new Schema({
     username: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
     },
     salt: {
-        type:String,
-        required: true
+        type: String,
+        // required: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     profileImageURL: {
-        type:String,
-        default: './public/default.png'
+        type: String,
+        default: './public/default.png',
     },
     role: {
         type: String,
         enum: ["USER","ADMIN"],
-        default: "USER"
+        default: "USER",
     }
 
 
 }, {timestamps: true});
 
+// mongoose pre function
+// It is used to save the password in hash form  
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if(!user.isModified("password")) return ;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+          .update(user.password)
+          .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+
+})
+
+// *******  //
+
 
 const User = model('user',userSchema);
 
-module.exports = {
-    User
-}
+module.exports = User;
 
 
